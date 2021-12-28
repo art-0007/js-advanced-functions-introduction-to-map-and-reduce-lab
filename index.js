e36f6acad4779b8e3a8fcee4a674e6fe26753eed
Artur <info@aigroupusa.com>
2021-12-28 12:58:50 -0500

    commit tree a657a72c6ac143dd361b4e75ec5cb24ae63dfef2

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+   return sourceArray.map(n => n * -1);
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(n => n);
+ }
+
+ function mapToDouble(sourceArray) {
+    return sourceArray.map(n => n * 2);
+ }
+
+ function mapToSquare(sourceArray) {
+    return sourceArray.map(n => n ** 2);
+ }
+
+ function reduceToTotal(sourceArray, startingPoint = 0) {
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+    return sourceArray.reduce(reducer, startingPoint)
+ }
+
+ function reduceToAllTrue(sourceArray) {
+    const reducer = function (previousValue, currentValue) {
+    if(!!previousValue == true && !!currentValue == true ){
+        return true;
+    } else {
+        return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+ }
+
+ function reduceToAnyTrue(sourceArray) {
+    const reducer = function (previousValue, currentValue) {
+    if(previousValue == true){
+        return true;
+    } else {
+        return !!currentValue;
+        }
+    }
+    return sourceArray.reduce(reducer, false)
+ }
